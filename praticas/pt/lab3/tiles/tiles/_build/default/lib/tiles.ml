82f59ddf832316d6c1712fc739fced60f7954e28
Sofia Barata <s.barata@campus.fct.unl.pt>
2026-03-18 20:14:24 +0000

    Pratical Class 3 - Exercise for AUTOLAB

--- praticas/pt/lab3/tiles/tiles/_build/default/lib/tiles.ml
+++ praticas/pt/lab3/tiles/tiles/_build/default/lib/tiles.ml
@@ -7,4 +7,4 @@ let rec tiles n =
    if n < 3 then 1 
   else if n = 3 then 2 
   else 1 + tiles(n-1)  (* Black tile first*)
-    + sum(fun i -> tiles (n-i-1)) 3 (n-1) (* Red tile first, blocks of 3,4,5,... *)
+    + sum(fun i -> tiles (n-i-1)) 3 (n-1) (* Red tile first, blocks of 3,4,5,...  *)
